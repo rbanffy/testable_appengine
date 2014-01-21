18be591aab73c6b4f9e9a1845f0f8474b9eadbab
Ciro Costa <ciro.costa@usp.br>
2014-01-21 14:49:12 -0200

    Added webtest to the testing environment. Basic handler also

--- tests/sanity_test.py
+++ tests/sanity_test.py
@@ -3,20 +3,43 @@ Ensure the environment is sane
 """
 
 import unittest
+import webtest
+
+from src import main
 from google.appengine.api import memcache
 from google.appengine.ext import db
 from google.appengine.ext import testbed
 
 
 class SanityTest(unittest.TestCase):
+    
     def setUp(self):
         self.testbed = testbed.Testbed()
         self.testbed.activate()
         self.testbed.init_datastore_v3_stub()
         self.testbed.init_memcache_stub()
+        self.testapp = webtest.TestApp(main.application)
 
     def tearDown(self):
         self.testbed.deactivate()
 
     def test_sanity(self):
         self.assertTrue(True)
+
+    def test_sample_request(self):
+        response = self.testapp.get('/')
+        self.assertEqual(response.status_int, 200)
+
+
+class HandlerTest(unittest.TestCase):
+    
+    def setUp(self):
+        self.testapp = webtest.TestApp(main.application)
+
+    def test_sample_request(self):
+        response = self.testapp.get('/')
+        self.assertEqual(response.status_int, 200)
+
+
+if __name__ == '__main__':
+    unittest.main()
