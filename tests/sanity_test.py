cb036ea9a6676f146332c56ed3506cd9fbbb231b
Ricardo Bánffy <rbanffy@gmail.com>
2014-01-21 12:43:18 -0200

    PEP-8 adjustment

--- tests/sanity_test.py
+++ tests/sanity_test.py
@@ -7,6 +7,7 @@ from google.appengine.api import memcache
 from google.appengine.ext import db
 from google.appengine.ext import testbed
 
+
 class SanityTest(unittest.TestCase):
     def setUp(self):
         self.testbed = testbed.Testbed()
