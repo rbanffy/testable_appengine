4fb2e6f765dc4dcdc111986933b42915ec00accf
Ricardo Bánffy <rbanffy@gmail.com>
2014-06-19 23:13:00 -0300

    Make handler test optional

--- tests/sanity_test.py
+++ tests/sanity_test.py
@@ -5,15 +5,21 @@ Ensure the environment is sane
 # Do not add your own tests to this file. This file is intended for the sole
 # purpose of confirming your environment was properly set-up
 
-
 import unittest
 import webtest
 
+# The test will error out if we can't import these items
 from google.appengine.api import memcache
 from google.appengine.ext import db
+from google.appengine.ext import ndb
 from google.appengine.ext import testbed
 
-from src import main
+# If your project has a src/main.py file, it'll check if it imports
+try:
+    from src import main
+    TEST_HANDLER = True
+except ImportError:
+    TEST_HANDLER = False
 
 
 class SanityTest(unittest.TestCase):
@@ -31,14 +37,15 @@ class SanityTest(unittest.TestCase):
         self.assertTrue(True)
 
 
-class HandlerTest(unittest.TestCase):
+if TEST_HANDLER:
+    class HandlerTest(unittest.TestCase):
 
-    def setUp(self):
-        self.testapp = webtest.TestApp(main.application)
+        def setUp(self):
+            self.testapp = webtest.TestApp(main.application)
 
-    def test_sample_request(self):
-        response = self.testapp.get('/')
-        self.assertEqual(response.status_int, 200)
+        def test_sample_request(self):
+            response = self.testapp.get('/')
+            self.assertEqual(response.status_int, 200)
 
 
 if __name__ == '__main__':
