bf6e1f0c6fc26386674424671899842c9ce9bd9d
Ricardo Bánffy <rbanffy@gmail.com>
2014-06-20 18:08:05 -0300

    Merging fixture loader test within sanity testing - we should not pollute the tests folder

--- tests/sanity_test.py
+++ tests/sanity_test.py
@@ -5,6 +5,7 @@ Ensure the environment is sane
 # Do not add your own tests to this file. This file is intended for the sole
 # purpose of confirming your environment was properly set-up
 
+import datetime
 import unittest
 import webtest
 
@@ -14,12 +15,26 @@ from google.appengine.ext import db
 from google.appengine.ext import ndb
 from google.appengine.ext import testbed
 
+from testable_appengine.loader import load_fixture
+
 # If your project has a src/main.py file, it'll check if it imports
 try:
     from src import main
     TEST_HANDLER = True
 except ImportError:
     TEST_HANDLER = False
+
+
+class Person(ndb.Model):
+    first_name = ndb.StringProperty()
+    last_name = ndb.StringProperty()
+    born = ndb.DateTimeProperty()
+    userid = ndb.IntegerProperty()
+    thermostat_set_to = ndb.FloatProperty()
+    snores = ndb.BooleanProperty()
+    started_school = ndb.DateProperty()
+    sleeptime = ndb.TimeProperty()
+    favorite_movies = ndb.JsonProperty()
 
 
 class SanityTest(unittest.TestCase):
@@ -36,6 +51,7 @@ class SanityTest(unittest.TestCase):
     def test_sanity(self):
         self.assertTrue(True)
 
+
 if TEST_HANDLER:
     class HandlerTest(unittest.TestCase):
 
@@ -45,6 +61,41 @@ if TEST_HANDLER:
         def test_sample_request(self):
             response = self.testapp.get('/')
             self.assertEqual(response.status_int, 200)
+
+
+class LoaderTest(unittest.TestCase):
+    def setUp(self):
+        self.testbed = testbed.Testbed()
+        self.testbed.activate()
+        self.testbed.init_datastore_v3_stub()
+        self.testbed.init_memcache_stub()
+        load_fixture('tests/persons.json', Person)
+
+    def tearDown(self):
+        self.testbed.deactivate()
+
+    def test_loaded(self):
+        # Test if the first record got in
+        person = Person.query(Person.first_name == 'John').get()
+        self.assertEqual(person.first_name, 'John')
+        self.assertEqual(person.last_name, 'Doe')
+        self.assertEqual(person.born, datetime.datetime(1968, 3, 3))
+        self.assertEqual(person.thermostat_set_to, 18.34)
+
+        # Test for the third one
+        person = Person.query(Person.last_name == 'Schneier' and
+                              Person.first_name == 'Alice').get()
+        self.assertEqual(person.first_name, 'Alice')
+        self.assertEqual(person.last_name, 'Schneier')
+        self.assertEqual(person.born, datetime.datetime(1999, 9, 19))
+        self.assertTrue(person.snores)
+
+        # Test for the last one
+        person = Person.query(
+            Person.born == datetime.datetime(1980, 5, 25, 0, 0, 0)).get()
+        self.assertEqual(person.first_name, 'Bob')
+        self.assertEqual(person.last_name, 'Schneier')
+        self.assertEqual(person.born, datetime.datetime(1980, 5, 25))
 
 
 if __name__ == '__main__':
