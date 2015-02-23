b3fd7d6aeae00a3c23a194c372cf4d20e22ed71c
Ricardo Bánffy <rbanffy@gmail.com>
2015-02-23 13:46:09 -0300

    Removed version detection anti-bug

--- resources/get_current_sdk_version.sh
+++ resources/get_current_sdk_version.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 
-# curl -s https://appengine.google.com/api/updatecheck | grep release | awk -F '\"' '{print $2}'
+curl -s https://appengine.google.com/api/updatecheck | grep release | awk -F '\"' '{print $2}'
 
-# Please see https://code.google.com/p/googleappengine/issues/detail?id=11604
-echo 1.9.17
