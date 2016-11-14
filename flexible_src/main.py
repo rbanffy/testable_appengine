b439ecb042682c1a073cf801d87f6087d70dd274
Ricardo Banffy <rbanffy@gmail.com>
2016-11-14 17:07:58 +0000

    Add flexible runtime support

---
+++ flexible_src/main.py
@@ -0,0 +1,12 @@
+import logging
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello world'
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=True)
